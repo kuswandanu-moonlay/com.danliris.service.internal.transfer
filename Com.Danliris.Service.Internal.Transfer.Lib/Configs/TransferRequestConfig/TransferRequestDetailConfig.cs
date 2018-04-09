9acde59626cb9b0c909211b26afb3f3921e8c612
achensetyawati <achenzetya@gmail.com>
2018-04-09 10:31:28 +0700

    TR config

---
+++ Com.Danliris.Service.Internal.Transfer.Lib/Configs/TransferRequestConfig/TransferRequestDetailConfig.cs
@@ -0,0 +1,22 @@
+﻿using Com.Danliris.Service.Internal.Transfer.Lib.Models.TransferRequestModel;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Danliris.Service.Internal.Transfer.Lib.Configs.TransferRequestConfig
+{
+    public class TransferRequestDetailConfig : IEntityTypeConfiguration<TransferRequestDetail>
+    {
+        public void Configure(EntityTypeBuilder<TransferRequestDetail> builder)
+        {
+            builder.Property(p => p.ProductId).HasMaxLength(255);
+            builder.Property(p => p.ProductCode).HasMaxLength(255);
+            builder.Property(p => p.ProductName).HasMaxLength(255);
+            builder.Property(p => p.ProductRemark).HasMaxLength(255);
+            builder.Property(p => p.UomId).HasMaxLength(255);
+            builder.Property(p => p.UomUnit).HasMaxLength(255);
+        }
+    }
+}
