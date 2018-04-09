b0304c69638b082b48b00f6ef5bad38d974fed89
achensetyawati <achenzetya@gmail.com>
2018-04-09 10:28:13 +0700

    viewModels

---
+++ Com.Danliris.Service.Internal.Transfer.Lib/ViewModels/TransferRequestViewModel/TransferRequestDetailViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Danliris.Service.Internal.Transfer.Lib.ViewModels.TransferRequestViewModel
+{
+    public class TransferRequestDetailViewModel
+    {
+        public ProductViewModel product { get; set; }
+        public double quantity { get; set; }
+        public UomViewModel uom { get; set; }
+        public string productRemark { get; set; }
+    }
+}
---
+++ Com.Danliris.Service.Internal.Transfer.Lib/ViewModels/TransferRequestViewModel/TransferRequestViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Danliris.Service.Internal.Transfer.Lib.ViewModels.TransferRequestViewModel
+{
+    public class TransferRequestViewModel
+    {
+        public string trNo { get; set; }
+        public string trDate { get; set; }
+        public string requestedArrivalDate { get; set; }
+        public UnitViewModel unit { get; set; }
+        public CategoryViewModel category { get; set; }
+        public bool isPosted { get; set; }
+        public bool isCanceled { get; set; }
+        public string remark { get; set; }
+        public List<TransferRequestDetailViewModel> details { get; set; }
+    }
+}
