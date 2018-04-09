a95dcf65e9b52b1e058f599e7ccd5c0b4cd660d1
achensetyawati <achenzetya@gmail.com>
2018-04-09 10:29:20 +0700

    TR api controller

---
+++ Com.Danliris.Service.Internal.Transfer.WebApi/Controllers/v1/TransferRequestController.cs
@@ -0,0 +1,53 @@
+﻿using Com.Danliris.Service.Internal.Transfer.Lib;
+using Com.Danliris.Service.Internal.Transfer.Lib.Models.TransferRequestModel;
+using Com.Danliris.Service.Internal.Transfer.Lib.PDFTemplates;
+using Com.Danliris.Service.Internal.Transfer.Lib.Services.TransferRequestService;
+using Com.Danliris.Service.Internal.Transfer.Lib.ViewModels.TransferRequestViewModel;
+using Com.Danliris.Service.Internal.Transfer.WebApi.Helpers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Com.Danliris.Service.Internal.Transfer.WebApi.Controllers.v1
+{
+    [Produces("application/json")]
+    [ApiVersion("1.0")]
+    [Route("v{version:apiVersion}/transfer-requests")]
+    [Authorize]
+    public class TransferRequestController : BasicController<InternalTransferDbContext, TransferRequestService, TransferRequestViewModel, TransferRequest>
+    {
+        private static readonly string ApiVersion = "1.0";
+        public TransferRequestController(TransferRequestService service) : base(service, ApiVersion)
+        {
+        }
+
+        [HttpGet("pdf/{Id}")]
+        public async Task<IActionResult> GetPdfById([FromRoute] int Id)
+        {
+            try
+            {
+                var model = await Service.ReadModelById(Id);
+                var viewModel = Service.MapToViewModel(model);
+
+                TransferRequestPDFTemplate PdfTemplate = new TransferRequestPDFTemplate();
+                MemoryStream stream = PdfTemplate.GeneratePdfTemplate(viewModel);
+
+                return new FileStreamResult(stream, "application/pdf")
+                {
+                    FileDownloadName = $"Transfer Request {viewModel.trNo}.pdf"
+                };
+            }
+            catch (Exception e)
+            {
+                Dictionary<string, object> Result =
+                    new ResultFormatter(ApiVersion, General.INTERNAL_ERROR_STATUS_CODE, e.Message)
+                    .Fail();
+                return StatusCode(General.INTERNAL_ERROR_STATUS_CODE, Result);
+            }
+        }
+    }
+}
