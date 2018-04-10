168d00ecfb807ec6c4f554e1b787be0d5fcd0a16
achensetyawati <achenzetya@gmail.com>
2018-04-10 14:06:47 +0700

    TR view models

--- Com.Danliris.Service.Internal.Transfer.Lib/ViewModels/TransferRequestViewModel/TransferRequestDetailViewModel.cs
+++ Com.Danliris.Service.Internal.Transfer.Lib/ViewModels/TransferRequestViewModel/TransferRequestDetailViewModel.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using Com.Danliris.Service.Internal.Transfer.Lib.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Com.Danliris.Service.Internal.Transfer.Lib.ViewModels.TransferRequestViewModel
 {
-    public class TransferRequestDetailViewModel
+    public class TransferRequestDetailViewModel : BasicViewModel
     {
         public ProductViewModel product { get; set; }
         public double quantity { get; set; }
--- Com.Danliris.Service.Internal.Transfer.Lib/ViewModels/TransferRequestViewModel/TransferRequestViewModel.cs
+++ Com.Danliris.Service.Internal.Transfer.Lib/ViewModels/TransferRequestViewModel/TransferRequestViewModel.cs
@@ -1,14 +1,16 @@
-﻿using System;
+﻿using Com.Danliris.Service.Internal.Transfer.Lib.Helpers;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Com.Danliris.Service.Internal.Transfer.Lib.ViewModels.TransferRequestViewModel
 {
-    public class TransferRequestViewModel
+    public class TransferRequestViewModel : BasicViewModel
     {
         public string trNo { get; set; }
-        public string trDate { get; set; }
-        public string requestedArrivalDate { get; set; }
+        public DateTime trDate { get; set; }
+        public DateTime requestedArrivalDate { get; set; }
         public UnitViewModel unit { get; set; }
         public CategoryViewModel category { get; set; }
         public bool isPosted { get; set; }
