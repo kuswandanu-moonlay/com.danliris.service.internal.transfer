bb0aa845eb62cc2f544dbf188f2198cf1d78b094
achensetyawati <achenzetya@gmail.com>
2018-04-16 13:36:07 +0700

    test-helpers

---
+++ Com.Danliris.Service.Internal.Transfer.Test/Helpers/General.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Danliris.Service.Internal.Transfer.Test.Helpers
+{
+    public static class General
+    {
+        public const string CONTROLLER_TEST_DATA = "Controller";
+        public const string SERVICE_TEST_DATA = "Service";
+    }
+}
