454dee558c1f50889c4c019f0b2d413076d1e11b
Lucia Widyana <mo_cia@yahoo.com>
2018-04-17 15:30:37 +0700

    viewModel

--- Com.Danliris.Service.Internal.Transfer.Lib/ViewModels/InternalTransferOrderViewModels/InternalTransferOrderDetailViewModel.cs
+++ Com.Danliris.Service.Internal.Transfer.Lib/ViewModels/InternalTransferOrderViewModels/InternalTransferOrderDetailViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Com.Danliris.Service.Internal.Transfer.Lib.Helpers;
+using Com.Danliris.Service.Internal.Transfer.Lib.Models.InternalTransferOrderModel;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -6,13 +7,36 @@ using System.Text;
 
 namespace Com.Danliris.Service.Internal.Transfer.Lib.ViewModels.InternalTransferOrderViewModels
 {
-    public class InternalTransferOrderDetailViewModel : BasicViewModel, IValidatableObject
+    public class InternalTransferOrderDetailViewModel : BasicViewModel//, IValidatableObject
     {
+
+        public string ProductId { get; set; }
+
+        public string ProductCode { get; set; }
+
+        public string ProductName { get; set; }
+
         public double Quantity { get; set; }
 
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            throw new NotImplementedException();
-        }
+        public int ITOId { get; set; }
+
+        public int TRDetailId { get; set; }
+
+        public string UomId { get; set; }
+
+        public string UomUnit { get; set; }
+
+        public string ProductRemark { get; set; }
+
+        public string Grade { get; set; }
+
+        public string Status { get; set; }
+
+
+
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    throw new NotImplementedException();
+        //}
     }
 }
--- Com.Danliris.Service.Internal.Transfer.Lib/ViewModels/InternalTransferOrderViewModels/InternalTransferOrderViewModel.cs
+++ Com.Danliris.Service.Internal.Transfer.Lib/ViewModels/InternalTransferOrderViewModels/InternalTransferOrderViewModel.cs
@@ -7,18 +7,45 @@ using System.Text;
 
 namespace Com.Danliris.Service.Internal.Transfer.Lib.ViewModels.InternalTransferOrderViewModels
 {
-    public class InternalTransferOrderViewModel : BasicViewModel, IValidatableObject
+    public class InternalTransferOrderViewModel : BasicViewModel//, IValidatableObject
     {
-        public string InternalTransferOrderNo { get; set; }
+        public string ITONo { get; set; }
 
-        public string TransferRequestNo { get; set; }
+        public int TRId { get; set; }
+
+        public string TRNo { get; set; }
+
+        public DateTime TRDate { get; set; }
+
+        public DateTime RequestedArrivalDate { get; set; }
 
         public string UnitId { get; set; }
 
+        public string UnitCode { get; set; }
+
+        public string UnitName { get; set; }
+
+        public string CategoryId { get; set; }
+
+        public string CategoryCode { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public string DivisionId { get; set; }
+
+        public string DivisionCode { get; set; }
+
+        public string DivisionName { get; set; }
+
+        public string Remarks { get; set; }
+
+        public bool IsPost { get; set; }
+        
         public List<InternalTransferOrderDetailViewModel> InternalTransferOrderDetails { get; set; }
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            throw new NotImplementedException();
-        }
+        ///public List<sourceTransferOrderViewModel> sourceTransferOrder { get; set; }
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    throw new NotImplementedException();
+        //}
     }
 }
