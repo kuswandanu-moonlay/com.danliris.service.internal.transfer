4847eb5955bd8f3fca4df550e3857bb7163da661
kuswandanu <kuswandanu@gmail.com>
2018-04-19 15:45:20 +0700

    create unused in InternalTransferOrder

--- Com.Danliris.Service.Internal.Transfer.WebApi/Controllers/v1/InternalTransferOrderController.cs
+++ Com.Danliris.Service.Internal.Transfer.WebApi/Controllers/v1/InternalTransferOrderController.cs
@@ -55,5 +55,28 @@ namespace Com.Danliris.Service.Internal.Transfer.WebApi.Controllers.v1
                 return StatusCode(General.INTERNAL_ERROR_STATUS_CODE, Result);
             }
         }
+
+        [HttpGet("unused")]
+        public IActionResult GetPostedTransferRequest(string Order = "{}", [Bind(Prefix = "Select[]")]List<string> Select = null, string Keyword = null, string Filter = "{}", [Bind(Prefix = "CurrentUsed[]")]List<int> CurrentUsed = null)
+        {
+            try
+            {
+                Service.Username = User.Claims.Single(p => p.Type.Equals("username")).Value;
+                List<InternalTransferOrder> Data = Service.ReadModelUnused(Order, Select, Keyword, Filter, CurrentUsed);
+
+                Dictionary<string, object> Result =
+                    new ResultFormatter(ApiVersion, General.OK_STATUS_CODE, General.OK_MESSAGE)
+                    .Ok(Data, Service.MapToViewModel);
+
+                return Ok(Result);
+            }
+            catch (Exception e)
+            {
+                Dictionary<string, object> Result =
+                    new ResultFormatter(ApiVersion, General.INTERNAL_ERROR_STATUS_CODE, e.Message)
+                    .Fail();
+                return StatusCode(General.INTERNAL_ERROR_STATUS_CODE, Result);
+            }
+        }
     }
 }
