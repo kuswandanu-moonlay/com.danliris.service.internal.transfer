4cc99e020db5312ce19d5438e168d573be7ab74a
kuswandanu <kuswandanu@gmail.com>
2018-04-23 11:05:39 +0700

    Add DivisionViewModel

---
+++ Com.Danliris.Service.Internal.Transfer.Lib/ViewModels/DivisionViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Com.Danliris.Service.Internal.Transfer.Lib.Helpers;
+
+namespace Com.Danliris.Service.Internal.Transfer.Lib.ViewModels
+{
+    public class DivisionViewModel : BasicViewModel
+    {
+        public string _id { get; set; }
+        public string code { get; set; }
+        public string name { get; set; }
+    }
+}
