d339223d7ffddcfc5f1c8e3e76920c480f5148cc
achensetyawati <achenzetya@gmail.com>
2018-04-26 13:40:39 +0700

    Add SO Config

---
+++ Com.Danliris.Service.Internal.Transfer.Lib/Configs/TransferShippingOrderConfigs/TransferShippingOrderDetailConfig.cs
@@ -0,0 +1,23 @@
+﻿using Com.Danliris.Service.Internal.Transfer.Lib.Models.TransferShippingOrderModel;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Danliris.Service.Internal.Transfer.Lib.Configs.TransferShippingOrderConfigs
+{
+    public class TransferShippingOrderDetailConfig : IEntityTypeConfiguration<TransferShippingOrderDetail>
+    {
+        public void Configure(EntityTypeBuilder<TransferShippingOrderDetail> builder)
+        {
+            builder.Property(p => p.ProductId).HasMaxLength(255);
+            builder.Property(p => p.ProductCode).HasMaxLength(255);
+            builder.Property(p => p.ProductName).HasMaxLength(255);
+            builder.Property(p => p.UomId).HasMaxLength(255);
+            builder.Property(p => p.UomUnit).HasMaxLength(255);
+            builder.Property(p => p.Grade).HasMaxLength(255);
+            builder.Property(p => p.ProductRemark).HasMaxLength(1000);
+        }
+    }
+}
