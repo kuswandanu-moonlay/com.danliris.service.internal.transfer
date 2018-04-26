d74c5f0db5246c06ca168cf7854fc28af6d47ad9
kuswandanu <kuswandanu@gmail.com>
2018-04-26 15:33:13 +0700

    Add IntegrationDataUtils

---
+++ Com.Danliris.Service.Internal.Transfer.Test/DataUtils/IntegrationDataUtils/CategoryDataUtil.cs
@@ -0,0 +1,28 @@
+﻿using Com.Danliris.Service.Internal.Transfer.Lib.Helpers;
+using Com.Danliris.Service.Internal.Transfer.Lib.ViewModels;
+using Com.Danliris.Service.Internal.Transfer.Test.Helpers;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Com.Danliris.Service.Internal.Transfer.Test.DataUtils.IntegrationDataUtils
+{
+    public static class CategoryDataUtil
+    {
+        public static CategoryViewModel GetCategory(HttpClientService client)
+        {
+            var response = client.GetAsync($"{APIEndpoint.Core}/master/categories?page=1&size=1").Result;
+            response.EnsureSuccessStatusCode();
+
+            var data = response.Content.ReadAsStringAsync();
+            Dictionary<string, object> result = JsonConvert.DeserializeObject<Dictionary<string, object>>(data.Result.ToString());
+
+            List<CategoryViewModel> list = JsonConvert.DeserializeObject<List<CategoryViewModel>>(result["data"].ToString());
+            CategoryViewModel category = list.First();
+
+            return category;
+        }
+    }
+}
---
+++ Com.Danliris.Service.Internal.Transfer.Test/DataUtils/IntegrationDataUtils/CurrencyDataUtil.cs
@@ -0,0 +1,29 @@
+﻿using Com.Danliris.Service.Internal.Transfer.Lib.Helpers;
+using Com.Danliris.Service.Internal.Transfer.Lib.ViewModels;
+using Com.Danliris.Service.Internal.Transfer.Test.Helpers;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Com.Danliris.Service.Internal.Transfer.Test.DataUtils.IntegrationDataUtils
+{
+    public static class CurrencyDataUtil
+    {
+        public static CurrencyViewModel GetCurrencyIDR(HttpClientService client)
+        {
+            var response = client.GetAsync($"{APIEndpoint.Core}master/currencies").Result;
+            response.EnsureSuccessStatusCode();
+
+            var data = response.Content.ReadAsStringAsync();
+            Dictionary<string, object> result = JsonConvert.DeserializeObject<Dictionary<string, object>>(data.Result.ToString());
+
+            List<CurrencyViewModel> list = JsonConvert.DeserializeObject<List<CurrencyViewModel>>(result["data"].ToString());
+            //CurrencyViewModel currency = list.First(p => p.code.Equals("IDR"));
+            CurrencyViewModel currency = list.First();
+
+            return currency;
+        }
+    }
+}
