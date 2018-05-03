50d04f2656ea0cdbd0918b456125930a6afcee0e
kuswandanu <kuswandanu@gmail.com>
2018-05-03 08:46:39 +0700

    Add Supplier and TransferShippingOrderItemsCount validation TSO

--- Com.Danliris.Service.Internal.Transfer.Lib/ViewModels/TransferShippingOrderViewModels/TransferShippingOrderViewModel.cs
+++ Com.Danliris.Service.Internal.Transfer.Lib/ViewModels/TransferShippingOrderViewModels/TransferShippingOrderViewModel.cs
@@ -1,11 +1,12 @@
 ﻿using Com.Danliris.Service.Internal.Transfer.Lib.Helpers;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Com.Danliris.Service.Internal.Transfer.Lib.ViewModels.TransferShippingOrderViewModels
 {
-    public class TransferShippingOrderViewModel : BasicViewModel
+    public class TransferShippingOrderViewModel : BasicViewModel, IValidatableObject
     {
         public string SONo { get; set; }
         public DateTime SODate { get; set; }
@@ -15,5 +16,15 @@ namespace Com.Danliris.Service.Internal.Transfer.Lib.ViewModels.TransferShipping
 
         public List<TransferShippingOrderItemViewModel> TransferShippingOrderItems { get; set; }
 
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (this.Supplier == null || string.IsNullOrWhiteSpace(this.Supplier._id))
+                yield return new ValidationResult("Supplier is required", new List<string> { "Supplier" });
+
+            if (TransferShippingOrderItems == null || TransferShippingOrderItems.Count.Equals(0))
+            {
+                yield return new ValidationResult("Transfer Shipping Order Item is required", new List<string> { "TransferShippingOrderItemsCount" });
+            }
+        }
     }
 }
